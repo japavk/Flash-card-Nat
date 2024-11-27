46a77f9a7fe23d431c81392601d5bfd43d285222
japa <00001104474980sp@al.educacao.sp.gov.br>
2024-11-27 18:16:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,48 @@
+criaCartao(
+    'Famosos',
+    'Quem é o cantor conhecido como "Rei do Pop"?',
+    'Michael Jackson'
+)
+
+criaCartao(
+    'Famosos',
+    'Quem foi o ator principal no filme "Piratas do Caribe"?',
+    'Johnny Depp'
+)
+
+criaCartao(
+    'Famosos',
+    'Quem é a cantora brasileira famosa pelo hit "Ai Se Eu Te Pego"?',
+    'Michel Teló'
+)
+
+criaCartao(
+    'Famosos',
+    'Quem foi o presidente dos Estados Unidos antes de Joe Biden?',
+    'Donald Trump.'
+)
+criaCartao(
+    'Famosos',
+    'Qual atriz ganhou o Oscar de Melhor Atriz em 2021 por sua performance em "Nomadland"?',
+    'Frances McDormand'
+)
+criaCartao(
+    'Famosos',
+    'Qual é o nome verdadeiro de Lady Gaga?',
+    'Stefani Joanne Angelina Germanotta.'
+)
+criaCartao(
+    'Famosos',
+    'Quem é o jogador de futebol argentino famoso por suas habilidades e sucesso no Barcelona, agora no PSG?',
+    'Lionel Messi'
+)
+criaCartao(
+    'Famosos',
+    'Quem é o famoso físico que desenvolveu a teoria da relatividade?',
+    'Albert Einstein'
+)
+criaCartao(
+    'Famosos',
+    'Qual atriz ficou famosa por interpretar Hermione Granger nos filmes de "Harry Potter"?',
+    'Emma Watson'
+)
